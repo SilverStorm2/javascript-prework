0c389b618a2a6468d130b3623a464953b7696006
Gabriela Gugulska-Sierant <ggs.amsterdam@gmail.com>
2024-08-22 05:05:42 +0200

    Add console.log statements for debugging and tracking function calls and variable states

--- js/script.js
+++ js/script.js
@@ -1,48 +1,62 @@
+// Funkcja zwracająca nazwę ruchu na podstawie ID
+function getMoveName(argMoveId) {
+  console.log('getMoveName called with:', argMoveId); // Sprawdza, z jakim argumentem wywoływana jest funkcja
+  if (argMoveId == 1) {
+    return 'kamień';
+  } else if (argMoveId == 2) {
+    return 'papier';
+  } else if (argMoveId == 3) {
+    return 'nożyce';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
+}
+
+// Funkcja wyświetlająca wynik gry
+function displayResult(argComputerMove, argPlayerMove) {
+  console.log('moves:', argComputerMove, argPlayerMove); // Wyświetlam oba ruchy na początku funkcji
+  if (argPlayerMove == 'nieznany ruch') {
+    console.log('Player made an invalid move'); // Sprawdzam, czy gracz wprowadził nieznany ruch
+    printMessage('Nieprawidłowy ruch - gra nie może zostać rozstrzygnięta.');
+  } else if (argComputerMove == argPlayerMove) {
+    console.log('It is a tie'); // Sprawdzam, czy jest remis
+    printMessage('Remis!');
+  } else if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+    console.log('Player wins with paper against rock'); // Sprawdzam, czy gracz wygrał papierem przeciwko kamieniowi
+    printMessage('Ty wygrywasz!');
+  } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+    console.log('Player wins with scissors against paper'); // Sprawdzam, czy gracz wygrał nożycami przeciwko papierowi
+    printMessage('Ty wygrywasz!');
+  } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+    console.log('Player wins with rock against scissors'); // Sprawdzam, czy gracz wygrał kamieniem przeciwko nożycom
+    printMessage('Ty wygrywasz!');
+  } else {
+    console.log('Player loses'); // Sprawdzam, czy gracz przegrał
+    printMessage('Przegrywasz!');
+  }
+}
+
 // Wpisanie wartości przez gracza
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-console.log('Gracz wpisał: ' + playerInput);
+console.log('Gracz wpisał:', playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
-if (playerInput == '1') {
-  playerMove = 'kamień';
-}
-if (playerInput == '2') {
-  playerMove = 'papier';
-}
-if (playerInput == '3') {
-  playerMove = 'nożyce';
-}
+console.log('Player move is:', playerMove); // Sprawdzam, jaka wartość została przypisana do playerMove
 
 printMessage('Twój ruch to: ' + playerMove);
 
 // Losowanie ruchu komputera
 let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Random number for computer move:', randomNumber); // Sprawdzam, jaka liczba została wylosowana dla komputera
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if (randomNumber == 1) {
-  computerMove = 'kamień';
-} else if (randomNumber == 2) {
-  computerMove = 'papier';
-} else if (randomNumber == 3) {
-  computerMove = 'nożyce';
-}
+console.log('Computer move is:', computerMove); // Sprawdzam, jaki ruch został przypisany do computerMove
 
 printMessage('Mój ruch to: ' + computerMove);
 
-// Określenie wyniku gry
-if (playerMove == 'nieznany ruch') {
-  printMessage('Nieprawidłowy ruch - gra nie może zostać rozstrzygnięta.');
-} else if (computerMove == playerMove) {
-  printMessage('Remis!');
-} else if (computerMove == 'kamień' && playerMove == 'papier') {
-  printMessage('Ty wygrywasz!');
-} else if (computerMove == 'papier' && playerMove == 'nożyce') {
-  printMessage('Ty wygrywasz!');
-} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-  printMessage('Ty wygrywasz!');
-} else {
-  printMessage('Przegrywasz!');
-}
+// Wywołanie funkcji wyświetlającej wynik gry
+displayResult(computerMove, playerMove);
